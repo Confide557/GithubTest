f1d03f70709bda3ae3b7d68b9c41366956f8f794
Alex Rodriguez <confide557@icloud.com>
2022-03-29 21:24:56 -0700

    Initial Commit

---
+++ GithubTest/GithubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestApp.swift
+//  GithubTest
+//
+//  Created by Alex Rodriguez on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
